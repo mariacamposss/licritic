547cb6c37878dc28e902ea0e77acb4d6d4e59915
Maria Luiza de Campos Santos <mluiza@ddcom.com.br>
2024-10-15 14:46:09 -0300

    privando rotas do navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'licritiqui';
 
   mostrarMenus: boolean = true;
+
+  constructor(
+    public router: Router,
+  ) {
+    router.events.forEach((event: any) => {
+      if (event instanceof NavigationStart) {
+
+        if (event['url'] === '/login' || event['url'] === '/error' ||
+          event['url'] === '**' || event['url'] === '/' || event["url"] === "error") {
+
+          this.mostrarMenus = false;
+        }
+      }
+    })
+
+
+  }  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
